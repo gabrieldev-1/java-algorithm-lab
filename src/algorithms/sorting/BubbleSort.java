41359077b5d70f85e11539972abfe12b1af4e043
gabrieldev-1 <developergabrieldossantos@gmail.com>
2026-01-05 21:27:18 -0300

    /sorting rename directory and optimization

--- src/algorithms/sorting/BubbleSort.java
+++ src/algorithms/sorting/BubbleSort.java
@@ -1,3 +1,5 @@
+package algorithms.sorting;
+
 import java.util.Random;
 import java.util.Arrays;
 
@@ -9,16 +11,34 @@ public class BubbleSort {
 	
 	}
 
+	/**
+	 * Sort a list of integers using bubble sort.
+	 * 
+	 * time complexity: O(n^2)
+	 * space complexity: 0(1)
+	 *
+	 * @param list list of integers
+	 * @return ordered list
+	 *
+	 */
 	public static void sort(int[] list) {
 		for(int i = 0; i < list.length; i++) {
+			Boolean swapped = false;
+
 			for(int j = 0; j < list.length - 1 - i; j++) {
 				if(list[j] > list[j + 1]) {
 					int temp = list[j];
 					list[j] = list[j + 1];
 					list[j + 1] = temp;
-				}
 
+					swapped = true;
+				}
 			}
+			
+			if(!swapped) {
+				break;
+			}
+
 		}
 	}
 
