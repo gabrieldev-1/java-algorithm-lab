3747156a06eaf3dd30ad01a2bc3d70b6670f3b8b
gabrieldev-1 <developergabrieldossantos@gmail.com>
2026-01-21 18:31:52 -0300

    Factorial exercise resolved

--- src/main/algorithms/recursion/Fibonacci.java
+++ src/main/algorithms/recursion/Fibonacci.java
@@ -1,12 +1,16 @@
 package algorithms.recursion;
 
 public class Fibonacci {
+    /**
+     * Calculate the value of position n in the Fibonacci sequence
+     * @param n position in sequence
+     * @return value of the position
+     */
     public static int calc(int n) {
         if(n <= 1) {
             return n;
         }
-
-        return calc(n - 1) + calc(n - 2); 
+        return calc(n - 2) + calc(n - 1);
 
     }
 }
