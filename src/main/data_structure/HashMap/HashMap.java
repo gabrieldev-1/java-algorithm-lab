4d6eefa46f96814c0cdae95c32821eacb0456120
gabrieldev-1 <developergabrieldossantos@gmail.com>
2026-02-23 11:25:35 -0300

    put and get add

--- src/main/data_structure/HashMap/HashMap.java
+++ src/main/data_structure/HashMap/HashMap.java
@@ -1,5 +1,7 @@
 package data_structure.HashMap;
 
+import java.util.NoSuchElementException;
+
 public class HashMap<K, V> {
 
     private static double LOAD_FACTOR = 0.75;
@@ -29,19 +31,62 @@ public class HashMap<K, V> {
         this.size = 0;
     }
 
+    private int index(K key) {
+        int hash = key.hashCode() & 0x7fffffff;
+        return hash % capacity;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
     private void resize() {
 
     }
     
     public Node<K, V> get(K key) {
+        try {
+
+            Node<K, V> element = table[index(key)];
+            
+            while (element.key != key) {
+                element = element.next;
+            }
+
+            return element;
+        
+        } catch (NoSuchElementException err) {
+            throw new NoSuchElementException("ERROR: The element not found. " + err.getMessage());
+
+        }
 
     }
 
     public void put(K key, V value) {
+        if(threshold >= size) {
+            resize();
+        }
+
+        Node<K, V> current = table[index(key)];
+
+        while (current != null) {
+            current = current.next;
+        }
+        current = new Node<K, V>(key, value);
 
     }
 
     public void remove(K key) {
+        try {
+            
+
+        } catch(NoSuchElementException err) {
+            throw new NoSuchElementException("ERROR: The element not found. " + err.getMessage());
+        }
+
+    }
+
+    public static void main(String[] args) {
 
     }
 }
